ecac7eca86d093807a43016a6d76017ffa50ac0b
ppadron5 <pedrompadron@gmail.com>
2018-04-24 14:22:09 +0000

    Initial Django Setup

---
+++ pythonzoo/zoo/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+	temporaryData = "Bill"
+	return render(
+		request,
+		"index.html",
+		context = { 'temporaryData' : temporaryData }
+    )
